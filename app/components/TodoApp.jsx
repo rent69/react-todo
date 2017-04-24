0401fc7cd15f4c0a42266a971ecee596972919bc
rent69 <rent.sidon@gmail.com>
2017-04-24 12:42:11 -0700

    Add test suite for list component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
+
 var TodoApp = React.createClass({
   getInitialState: function () {
     return {
@@ -20,12 +22,17 @@ var TodoApp = React.createClass({
       ]
     };
   },
+  handleAddTodo: function (text) {
+    alert('new todo: ' + text);
+  },
   render: function () {
     var {todos} = this.state;
+    var {newTodo} = this.props;
 
     return (
       <div>
         <TodoList todos={todos}/>
+        <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     )
   }
