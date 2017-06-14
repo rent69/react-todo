1111149b4c279e96cb9c1d294b101195a12c141e
rent69 <rent.sidon@gmail.com>
2017-06-14 11:57:17 -0700

    Initial Commit

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -64,6 +64,7 @@ describe('Reducers', () => {
           expect(res[0].completedAt).toEqual(updates.completedAt);
           expect(res[0].text).toEqual(todos[0].text);
       });
+      
       it('should add existing todos', () => {
         var todos = [{
           id: '111',
@@ -81,6 +82,22 @@ describe('Reducers', () => {
         expect(res.length).toEqual(1);
         expect(res[0]).toEqual(todos[0]);
       });
+
+      it('should wipe todos on logout', () => {
+        var todos = [{
+          id: '111',
+          text: 'anything',
+          completed: false,
+          completedAt: undefined,
+          createdAt: 33000
+        }];
+        var action = {
+          type: 'LOGOUT'
+        };
+        var res = reducers.todosReducer(df(todos), df(action));
+
+        expect(res.length).toEqual(0);
+      });
     });
 
     describe('authReducer', () => {
@@ -95,10 +112,11 @@ describe('Reducers', () => {
           uid: action.uid
         });
       });
+
       it('should wipe auth on LOGOUT', () => {
         const authData = {
           uid: '123abc'
-        }
+        };
         const action = {
           type: 'LOGOUT'
         };
